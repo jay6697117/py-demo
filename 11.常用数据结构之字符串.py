f88bb912bc1d117e2f8010c51cb49e7fef631033
SteveZhang <871506263@qq.com>
2025-07-22 10:26:51 +0800

    1

---
+++ 11.常用数据结构之字符串.py
@@ -0,0 +1,13 @@
+s = 'abc123456'
+n = len(s) # 9
+print(n)
+# print(s[0], s[-n])    # a a
+# print(s[n-1], s[-1])  # 6 6
+# print(s[2], s[-7])    # c c
+# print(s[5], s[-4])    # 3 3
+# print(s[2:5])         # c12
+# print(s[-7:-4])       # c12
+# print(s[2:])          # c123456
+# print(s[:2])          # ab
+# print(s[::2])         # ac246
+# print(s[::-1])        # 654321cba
