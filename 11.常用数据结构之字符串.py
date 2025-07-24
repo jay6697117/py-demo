ac4017ffcada0040a430e7e0960b0637cf9d22ea
SteveZhang <871506263@qq.com>
2025-07-24 10:07:22 +0800

    1

--- 11.常用数据结构之字符串.py
+++ 11.常用数据结构之字符串.py
@@ -1,13 +1,10 @@
-s = 'abc123456'
-n = len(s) # 9
-print(n)
-# print(s[0], s[-n])    # a a
-# print(s[n-1], s[-1])  # 6 6
-# print(s[2], s[-7])    # c c
-# print(s[5], s[-4])    # 3 3
-# print(s[2:5])         # c12
-# print(s[-7:-4])       # c12
-# print(s[2:])          # c123456
-# print(s[:2])          # ab
-# print(s[::2])         # ac246
-# print(s[::-1])        # 654321cba
+# 可以通过字符串的startswith、endswith来判断字符串是否以某个字符串开头和结尾；还可以用is开头的方法判断字符串的特征，这些方法都返回布尔值，代码如下所示。
+
+s1 = 'hello, world!'
+print(s1.startswith('He'))   # False
+print(s1.startswith('hel'))  # True
+print(s1.endswith('!'))      # True
+s2 = 'abc123456'
+print(s2.isdigit())  # False
+print(s2.isalpha())  # False
+print(s2.isalnum())  # True
