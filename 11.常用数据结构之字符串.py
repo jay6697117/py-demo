b81c4b62cd4155dd9681873a1d72cf1038b35101
SteveZhang <871506263@qq.com>
2025-07-25 10:37:06 +0800

    1

--- 11.常用数据结构之字符串.py
+++ 11.常用数据结构之字符串.py
@@ -1,10 +1,3 @@
-# 可以通过字符串的startswith、endswith来判断字符串是否以某个字符串开头和结尾；还可以用is开头的方法判断字符串的特征，这些方法都返回布尔值，代码如下所示。
-
-s1 = 'hello, world!'
-print(s1.startswith('He'))   # False
-print(s1.startswith('hel'))  # True
-print(s1.endswith('!'))      # True
-s2 = 'abc123456'
-print(s2.isdigit())  # False
-print(s2.isalpha())  # False
-print(s2.isalnum())  # True
+a = 321
+b = 123
+print(f'{a} * {b} = {a * b}')
